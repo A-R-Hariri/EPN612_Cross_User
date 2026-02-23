dc04a5b3ae98a43460dd7d4ae7adb4f8457f01c8
A-R-Hariri <a.r.hariri2000@gmail.com>
2026-02-22 23:21:45 -0400

    main scripts

---
+++ emb.py
@@ -0,0 +1,86 @@
+import warnings, sys, os, gc
+from os.path import join
+warnings.filterwarnings("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+import torch; print(torch.cuda.is_available())
+
+import libemg
+from libemg.datasets import get_dataset_list
+from libemg.feature_extractor import FeatureExtractor
+
+import numpy as np, pandas as pd
+import random, copy, time
+from sklearn.utils.class_weight import compute_class_weight
+import matplotlib.pyplot as plt
+
+from utils import *
+from models import *
+
+SEED = 13; random.seed(SEED); np.random.seed(SEED)
+GENERATOR = torch.manual_seed(SEED)
+MMAP_MODE = 'r'; SAVE_CHKP = True
+
+
+# ======== LOAD DATA ========
+train_data = np.load(join(PICKLE_PATH, 'train_data.npy'), allow_pickle=True).item()
+val_data = np.load(join(PICKLE_PATH, 'val_data.npy'), allow_pickle=True).item()
+test_data = np.load(join(PICKLE_PATH, 'test_data.npy'), allow_pickle=True).item()
+
+train_windows = np.load(join(PICKLE_PATH, 'train_windows.npy'), mmap_mode=MMAP_MODE)
+train_meta = np.load(join(PICKLE_PATH, 'train_meta.npy'), allow_pickle=True).item()
+val_windows = np.load(join(PICKLE_PATH, 'val_windows.npy'), mmap_mode=MMAP_MODE)
+val_meta = np.load(join(PICKLE_PATH, 'val_meta.npy'), allow_pickle=True).item()
+test_windows = np.load(join(PICKLE_PATH, 'test_windows.npy'), mmap_mode=MMAP_MODE)
+test_meta = np.load(join(PICKLE_PATH, 'test_meta.npy'), allow_pickle=True).item()
+
+test_windows_segmented = np.load(join(PICKLE_PATH, 'test_windows_segmented.npy'), mmap_mode=MMAP_MODE)
+test_meta_segmented = np.load(join(PICKLE_PATH, 'test_meta_segmented.npy'), allow_pickle=True).item()
+test_windows_relabeled = np.load(join(PICKLE_PATH, 'test_windows_relabeled.npy'), mmap_mode=MMAP_MODE)
+test_meta_relabeled = np.load(join(PICKLE_PATH, 'test_meta_relabeled.npy'), allow_pickle=True).item()
+
+# ======== PIPELINE ========
+
+train_loader = create_triplet_loader(
+    train_windows, train_meta['classes'], train_meta['subjects'],
+    batch=BATCH_SIZE, n_classes=5, n_subjects=2)
+val_loader_triplet = create_triplet_loader(
+    val_windows, val_meta['classes'], val_meta['subjects'],
+    batch=BATCH_SIZE, n_classes=5, n_subjects=2)
+
+test_loader = create_loader(test_windows, test_meta['classes'], 
+                            batch=BATCH_SIZE, shuffle=False)
+test_loader_segmented = create_loader(test_windows_segmented, 
+                            test_meta_segmented['classes'], 
+                            batch=BATCH_SIZE, shuffle=False)
+test_loader_relabeled = create_loader(test_windows_relabeled, 
+                            test_meta_relabeled['classes'],
+                            batch=BATCH_SIZE, shuffle=False)
+
+
+NAME = "cnn_triplet"
+model = CNN()
+print(model, f"\nParameters count: {count_params(model):,}")
+weights = torch.tensor(compute_class_weight('balanced', 
+                               classes=np.arange(CLASSES), 
+                                y=train_meta['classes']),
+                                dtype=torch.float32,
+                                device=DEVICE)
+train_triplet(model=model, name=NAME, 
+      train_loader=train_loader,
+      val_loader=val_loader_triplet, 
+      criterion_ce=nn.CrossEntropyLoss(weights),
+      criterion_tri=TripletLoss(),
+      tr_phase1=0.95, ce_phase1=0.05,
+      tr_phase2=0.5, ce_phase2=1.0,
+      epochs_phase1=0, save_chkp=SAVE_CHKP)
+
+torch.save(model.state_dict(), join(CHECKPOINT_PATH, NAME, f"{NAME}.pt"))
+# model.load_state_dict(torch.load(join(CHECKPOINT_PATH, NAME, "f{NAME}.pt")))
+eval_test(model=model, name=NAME, 
+          loaders={'raw': test_loader, 
+                   'segmented': test_loader_segmented, 
+                   'relabeled': test_loader_relabeled},
+           metas={'raw': test_meta, 
+                  'segmented': test_meta_segmented, 
+                  'relabeled': test_meta_relabeled})
---
+++ within.py
@@ -0,0 +1,91 @@
+import warnings, sys, os, gc
+from os.path import join
+warnings.filterwarnings("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+import torch; print(torch.cuda.is_available())
+
+import libemg
+from libemg.datasets import get_dataset_list
+from libemg.feature_extractor import FeatureExtractor
+
+import numpy as np, pandas as pd
+import random, copy, time
+from sklearn.utils.class_weight import compute_class_weight
+import matplotlib.pyplot as plt
+
+from utils import *
+from models import *
+
+SEED = 13; random.seed(SEED); np.random.seed(SEED)
+GENERATOR = torch.manual_seed(SEED)
+MMAP_MODE = 'r'; SAVE_CHKP = False
+
+# ======== LOAD DATA ========
+train_data = np.load(join(PICKLE_PATH, 'train_data.npy'), allow_pickle=True).item()
+val_data = np.load(join(PICKLE_PATH, 'val_data.npy'), allow_pickle=True).item()
+test_data = np.load(join(PICKLE_PATH, 'test_data.npy'), allow_pickle=True).item()
+
+train_windows = np.load(join(PICKLE_PATH, 'train_windows.npy'), mmap_mode=MMAP_MODE)
+train_meta = np.load(join(PICKLE_PATH, 'train_meta.npy'), allow_pickle=True).item()
+val_windows = np.load(join(PICKLE_PATH, 'val_windows.npy'), mmap_mode=MMAP_MODE)
+val_meta = np.load(join(PICKLE_PATH, 'val_meta.npy'), allow_pickle=True).item()
+test_windows = np.load(join(PICKLE_PATH, 'test_windows.npy'), mmap_mode=MMAP_MODE)
+test_meta = np.load(join(PICKLE_PATH, 'test_meta.npy'), allow_pickle=True).item()
+
+
+# Within
+NAME = 'cnn_raw_within'
+results = []
+
+ranges = [(0, 306), (306, 332), (332, 612)]
+data_list = [train_data, val_data, test_data]
+
+for d, r in enumerate(ranges):
+    for i in range(*r):
+        print(i)
+
+        data_s = data_list[d].isolate_data("subjects", [i], fast=True)
+
+        data = data_s.isolate_data("reps", list(range(15)), fast=True)
+        train_windows, train_meta = data.parse_windows(SEQ, INC)
+
+        data = data_s.isolate_data("reps", list(range(15, 20)), fast=True)
+        val_windows, val_meta = data.parse_windows(SEQ, INC)
+
+        data = data_s.isolate_data("reps", list(range(20, 25)), fast=True)
+        test_windows, test_meta = data.parse_windows(SEQ, INC)
+
+        weights = torch.tensor(compute_class_weight('balanced', 
+                                    classes=np.arange(CLASSES), 
+                                        y=train_meta['classes']),
+                                        dtype=torch.float32,
+                                        device=DEVICE)
+
+        train_loader = create_loader(train_windows, train_meta['classes'], 
+                                    batch=BATCH_SIZE, shuffle=True)
+        val_loader = create_loader(val_windows, val_meta['classes'], 
+                                    batch=BATCH_SIZE, shuffle=False)
+        test_loader = create_loader(test_windows, test_meta['classes'], 
+                                    batch=BATCH_SIZE, shuffle=False)
+
+        model = CNN()
+        weights = torch.tensor(compute_class_weight('balanced', 
+                                    classes=np.arange(CLASSES), 
+                                        y=train_meta['classes']),
+                                        dtype=torch.float32,
+                                        device=DEVICE)
+        train(model=model, name=NAME, 
+            train_loader=train_loader,
+            val_loader=val_loader, 
+            loss_fn=nn.CrossEntropyLoss(weight=weights),
+            save_chkp=SAVE_CHKP, verbose=False)
+        _result = eval_within(model=model, name=NAME, 
+                            loader=test_loader,
+                            meta=test_meta)
+        results.append(_result)
+        print(_result['acc_mean'])
+
+os.makedirs(f"{CHECKPOINT_PATH}", exist_ok=True)
+os.makedirs(f"{CHECKPOINT_PATH}/{NAME}/", exist_ok=True)
+np.save(f"{CHECKPOINT_PATH}/{NAME}/results.npy", results)
