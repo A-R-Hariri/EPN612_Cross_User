0e1501f500202bdb93cc035a4a5f1dea712b7f4d
A-R-Hariri <a.r.hariri2000@gmail.com>
2026-06-02 20:33:13 -0300

    fixed sysargs

--- within_mhcnn.py
+++ within_mhcnn.py
@@ -28,8 +28,7 @@ MMAP_MODE = 'r'; SAVE_CHKP = False
 
 
 TAG = sys.argv[2] if len(sys.argv) > 2 else "raw"
-REPS = sys.argv[3].split(',') if len(sys.argv) > 3 else 15
-REPS = list(map(int, REPS))
+REPS = set(sys.argv[3:]) if len(sys.argv) > 3 else {15}
 FT = sys.argv[4] == 'ft' if len(sys.argv) > 4 else False
 
 # ======== LOAD DATA ========
